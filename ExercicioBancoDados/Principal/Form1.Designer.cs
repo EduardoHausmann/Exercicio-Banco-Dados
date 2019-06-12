1b0f21586821610a0e5a201303dd8fad53b989fc
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-06-12 20:22:14 -0300

    Criados os Botoes para os outros Formularios

--- ExercicioBancoDados/Principal/Form1.Designer.cs
+++ ExercicioBancoDados/Principal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Principal
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPeixes = new System.Windows.Forms.Button();
+            this.btnColaboradores = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPeixes
+            // 
+            this.btnPeixes.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeixes.Location = new System.Drawing.Point(30, 60);
+            this.btnPeixes.Name = "btnPeixes";
+            this.btnPeixes.Size = new System.Drawing.Size(154, 58);
+            this.btnPeixes.TabIndex = 0;
+            this.btnPeixes.Text = "Peixes";
+            this.btnPeixes.UseVisualStyleBackColor = true;
+            // 
+            // btnColaboradores
+            // 
+            this.btnColaboradores.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColaboradores.Location = new System.Drawing.Point(236, 60);
+            this.btnColaboradores.Name = "btnColaboradores";
+            this.btnColaboradores.Size = new System.Drawing.Size(154, 58);
+            this.btnColaboradores.TabIndex = 1;
+            this.btnColaboradores.Text = "Colaboradores";
+            this.btnColaboradores.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.Location = new System.Drawing.Point(442, 60);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(154, 58);
+            this.btnClientes.TabIndex = 2;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(644, 151);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnColaboradores);
+            this.Controls.Add(this.btnPeixes);
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPeixes;
+        private System.Windows.Forms.Button btnColaboradores;
+        private System.Windows.Forms.Button btnClientes;
     }
 }
 
