78e9ec174e8ca1ae700ca9b28efafe7f83a28b48
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-06-14 11:19:48 -0300

    Alterado o limite do Salario

--- ExercicioBancoDados/Principal/EstruturaColaboradores.sql
+++ ExercicioBancoDados/Principal/EstruturaColaboradores.sql
@@ -3,7 +3,7 @@ CREATE TABLE colaboradores(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR(50),
 	cpf VARCHAR(14),
-	salario DECIMAL(6,2),
+	salario DECIMAL(10,2),
 	sexo VARCHAR(10),
 	cargo VARCHAR(45),
 	programador BIT
